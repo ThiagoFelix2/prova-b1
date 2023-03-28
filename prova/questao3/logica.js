988aac92a77ba7ed11ea843bb6bc7af0c15c6bc0
ThiagoFelix2 <104323042+ThiagoFelix2@users.noreply.github.com>
2023-03-27 21:39:32 -0300

    Add files via upload

---
+++ prova/questao3/logica.js
@@ -0,0 +1,16 @@
+var nome = document.getElementById('nome')
+var sobrenome = document.getElementById('sobrenome')
+
+	
+	function validar(nome, sobrenome){
+
+		
+
+		if(nome = ''){
+
+			alert('Erro insira seus dados para validar suas informações') 
+
+		} else {
+	
+	document.write('Seja bem vindo')
+	}
